c0b070ad4c31dbeca8bd5d21b1e1b8a127ec364d
Joey Smith <jsmith@webinertia.net>
2023-11-14 07:50:52 -0600

    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- Sources/Db/DatabaseApi.php
+++ Sources/Db/DatabaseApi.php
@@ -362,7 +362,7 @@ abstract class DatabaseApi
 		// Double check that we found what we expected.
 		if (!(self::$db instanceof DatabaseApi))
 		{
-			unset($self::$db);
+			unset(self::$db);
 			ErrorHandler::displayDbError();
 		}
 

29fc68ad5ed805fd07b32478831d29066c80e138
Joey Smith <jsmith@webinertia.net>
2024-01-04 18:16:32 -0600

    AddActionListener successfully adds to Form::$actions array Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- Sources/Mods/Demo/AddActionListener.php
+++ Sources/Mods/Demo/AddActionListener.php
@@ -4,15 +4,20 @@ declare(strict_types=1);
 
 namespace SMF\Mods\Demo;
 
+use SMF\Events\IntegrationEvent;
+
 final class AddActionListener
 {
+	// 'agreement' => ['', 'SMF\\Actions\\Agreement::call'],
 	public function __construct()
 	{
 
 	}
 
-	public function __invoke()
+	public function __invoke(IntegrationEvent $event)
 	{
-
+		$customAction = ['', static::class];
+		$target = $event->getTarget();
+		$target::$actions += ['demo' => $customAction];
 	}
 }
---
+++ Sources/Mods/Demo/DemoAction.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SMF\Mods\Demo;
+
+final class DemoAction
+{
+	public function __construct()
+	{
+
+	}
+
+	public function __invoke()
+	{
+
+	}
+}

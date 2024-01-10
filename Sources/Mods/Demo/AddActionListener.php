40ae3c94c0688f8c8d6fb6e7b93f9030ff3ea99b
Joey Smith <jsmith@webinertia.net>
2024-01-09 22:55:59 -0600

    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- Sources/Mods/Demo/AddActionListener.php
+++ Sources/Mods/Demo/AddActionListener.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 namespace SMF\Mods\Demo;
 
 use SMF\Events\IntegrationEvent;
+use SMF\Forum;
 use SMF\Mods\Demo\DemoAction;
 
 final class AddActionListener
 {
-	// 'agreement' => ['', 'SMF\\Actions\\Agreement::call'],
 	public function __construct()
 	{
 
@@ -17,6 +17,7 @@ final class AddActionListener
 
 	public function __invoke(IntegrationEvent $event)
 	{
+		/** @var Forum */
 		$target   = $event->getTarget();
 		$instance = new DemoAction();
 		$target::$actions += ['demo' => ['', [$instance, '__invoke']]];
